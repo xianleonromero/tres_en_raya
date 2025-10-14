e0637425be537c161d21f4e67e0b51815aa0900f
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2025-10-14 14:57:48 +0200

    Primer componente y css aplicado

---
+++ src/components/App/index.js
@@ -0,0 +1,6 @@
+function App() {
+  return(
+    <h1>React</h1>
+  )
+}
+export {App};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import {App} from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>React</h1>
+    <App />
   </React.StrictMode>
 );
