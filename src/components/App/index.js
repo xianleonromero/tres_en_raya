8720af69abce3ff5f2eda38e07f25a950be8717a
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2025-10-14 22:43:11 +0200

    Añadida la funcionalidad del juego

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,6 +11,7 @@ function App() {
             null,null,null
         ]
     )
+    const win=calculateWinner();
     const onClickCell= (index) =>{
         if (table[index]===null){
             const newTable=table.slice();
@@ -21,9 +22,32 @@ function App() {
                 : setPlayer('X')
         }
     }
+    function calculateWinner(){
+        const lines=[
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        for(let i=0;i<lines.length;i++){
+            const [a,b,c]=lines[i];
+            if (
+                table[a] && 
+                table[a] === table[b] && 
+                table[a] === table[c]
+            ){
+                return table[a];
+            }
+        }
+        return null;
+    }
   return(
     <main className="App">
-        <Modal />
+        {!!win && <Modal/>}
         <h1 className="App-title">3 en raya- Turno de {player}</h1>
         <Game player={player} onClickCell={onClickCell} table={table} />
     </main>
